2e16b4c983a7338231656fdcc665877ce414a8a8
Helena Saborit <helena.sb91@gmail.com>
2022-04-02 12:30:55 +0200

    working on snake

--- js/index.js
+++ js/index.js
@@ -8,13 +8,50 @@ ctx.lineWidth = 10;
 ctx.strokeStyle = 'rgba(38, 42, 38, 0.85)'
 ctx.strokeRect((canvas.width / 2) - 400, 0, 800, 750)
 
+document.getElementById('start-button').addEventListener('click', () =>myGame.startGame());
+class Game {
+  constructor () {
+      this.snake = null;
+      this.apples = null
+  }
 
-ctx.fillText("SCORE: ", 250, 400)
- 
-document.getElementById('start-button').addEventListener('click', startGame);
-
-function startGame() {
+  startGame() {
     console.log("click");
+    this.snake = new Snake(this.ctx, 50, 50, 50, 50);
+    this.createEventListeners();
+    //this.apples = new Apples();
 }
 
+    createEventListeners(){
+        document.addEventListener('keydown', (e) => {
+            switch (e.key) {
+                case "ArrowLeft" :
+                    this.snake.moveLeft()
+                    break;
+                case "ArrowRight" :
+                    this.snake.moveRight()
+                    break;
+                case "ArrowUp" :
+                    this.snake.moveUp()
+                    break;
+                case "ArrowDown" :
+                    this.snake.moveDown()
+                    break;
+            }
+        })
+}
+  }
 
+const myGame = new Game();
+
+
+
+
+ 
+
+
+/*
+function createPlayer() {
+  const snake = new SnakeCanvas(this.ctx, 50, 50, 50, 50)
+
+} */
--- js/snake.js
+++ js/snake.js
@@ -1,23 +1,59 @@
-class SnakeCanvas{
-    constructor(){
-        this.canvas = null;
-        this.ctx = null;
-        
-        this.snake = {w: null, h: null};
+class Snake{
+    constructor(canvas, ctx, positionX, positionY, width, height){
+        this.canvas = canvas;
+        this.ctx = ctx;
+        this.snakePosition = {x: positionX, y: positionY}
+        this.snakeSize = {w: width, h: height}
+        this.snakeSpeed = 2;
+        this.snake = null             
+        this.draw()
         
     }
-   //should i make a CLASS for game?
+  
+    init (){
+        
+    }
 
-draw(){
-     //this should be in another class/js?
-     ctx.rect(600, 100, 20, 20);
-     ctx.fillStyle= "white";
-     ctx.fill();    
-     ctx.strokeStyle = "black";
-     ctx.stroke();
+    draw(){    
+        //head
+
+
+        // body
+        ctx.rect(600, 100, 20, 20);
+        ctx.fillStyle= "white";
+        ctx.fill();    
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = "black";          
+        ctx.stroke();
+
+    }
+
+    moveRight(){
+        if (this.snakePosition.x < this.canvas) {
+        this.snakePosition.x += this.snakeSpeed
+        console.log("moveright")
+        } 
+    }
+    
+    moveLeft(){
+        if (this.snakePosition.x > 0) {
+        this.snakePosition.x -= this.snakeSpeed
+        console.log("move left")
+        } else if (this.snakePosition.x === 0) {
+            console.log("dead")
+            //DEAD
+    }}
+
+    moveUp(){
+        if(this.snakePosition.y < 0){
+        this.snakePosition.y -= this.snakeSpeed //NOT SURE
+        console.log("move up")
+        }
+    }
+    moveDown(){
+        this.snakePosition.y -= this.snakeSpeed //NOT SURE
+        console.log("move down")
+    }
 }
 
- init (){
-
- }
-}
+//movement & colisions 
